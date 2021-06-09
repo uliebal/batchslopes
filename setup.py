a68c4e85a836cc1f378f8ae91f0d6fa38d66b4e3
Ulf Liebal <ulf.liebal@rwth-aachen.de>
2021-06-09 18:11:56 +0200

    delete analysis, src focus only

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='batchslopes',
-    version='0.0.5',
+    version='0.0.7',
     author='Ulf Liebal',
     author_email='ulf.liebal@rwth-aachen.de',
     description='batchslopes is a tool to identify the growth rate in growth profiler data. The data is partitioned into decreasing set sizes until the highest correlation coefficient of exponential growth is detected.',
@@ -10,21 +10,8 @@ setuptools.setup(
     url='https://git.rwth-aachen.de/ulf.liebal/batchslopes.git',
     package_dir={'': 'src'},
     packages=setuptools.find_packages(where='src'),
-    classifiers=[
-        'Development Status :: 1 - Development/Unstable',
-        'Intended Audience :: Biotechnology Research',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3 :: Only',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
     python_requires='>=3.6',
     install_requires=[
-        'numpy>=1.18.0',
-        'pandas>=1.1.3'
+        'numpy>=1.18.0'
     ],
 )
